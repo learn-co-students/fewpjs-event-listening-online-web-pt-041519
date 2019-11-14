aa42edb3be35488775ea2d6c8010d848d28795a9
github username <github email address>
2019-11-14 06:01:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+
 function addingEventListener() {
-   const input = document.getElementById('input');
-   input.addEventListener('click', function(event) {
-      alert('I was clicked');
-   });
+// set a variable for the input node
+const input = document.getElementById('input')
+
+// now that we have an input, we want to do something with it
+// in this case, the type of event will be a click
+// when that click takes place, we will alert the browser it was clicked
+// we pass event listener the type of event, then the callback function 
+
+input.addEventListener('click', function(alert) {
+   alert('I was clicked!')
+})
+
 }

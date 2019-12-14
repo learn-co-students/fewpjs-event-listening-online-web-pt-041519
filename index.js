b723ddf0c91254bc0db2e6a3b816b0b3086915a9
kimpham54 <kimpham54@gmail.com>
2019-12-14 16:24:37 -0700

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function addingEventListener() {
     return "clicked"
   });
 }
+
+// const input = document.getElementById('input');
+// input.addEventListener('click', function(event) {
+//   alert('I was clicked!');
+// });
